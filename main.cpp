3267f4342ccca7a1d7dede22a4f6abefca9b8f6d
Kociarz <dbednorz@tuta.io>
2025-12-27 16:08:28 +0100

    Minor changes
    Added the base code for opening files in main.cpp
    Added multiple comments explaining the code
    Updated .gitignore file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    std::ifstream is("testimage.jpg", std::ios::binary);
+    int length = 10;
+
+    //TODO: Implement conversion from bytes to hexadecimal numbers 
+
+    if (is) {
+        std::cout << "opened the file successfully" << std::endl;
+        char * buffer = new char[length]; //Raw bytes are being stored here
+        is.read(buffer, length);
+        std::cout << buffer; //If the bytes are not converted to hexadecimal, this will output ASCII nonsense
+    }
+    else{
+        std::cout << "failed to open the file!" << std::endl;
+    }
+
     return 0;
 }
