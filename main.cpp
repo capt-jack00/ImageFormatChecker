96df16479a25e77c7a68738b29724337204bd953
Tytus <dbednorz@tuta.io>
2026-01-12 12:04:21 +0100

    Code readability improvement
    Added more detailed comments. Removed unused variables. No major code changes. Deleted unnecessary comments.

--- main.cpp
+++ main.cpp
@@ -5,28 +5,24 @@
 #include <map>
 #include "include/bintohex.h"
 
-//TODO: Write a detailed documentation for the code.
 
 int main(int argc, char* argv[]){
-    // Arguments
     std::string imgPath;
-    if(argc == 2){
-        imgPath = argv[1];
+    if(argc == 2){                                                                  //This argc needs to be equal to number 2 because executing command "./main" also counts as an argument
+        imgPath = argv[1];                                                          //The second argument equals an image path
     }
     else{
         std::cout << "Incorrect amount of arguments provided!" << std::endl;
         return 0;
     }
 
-    //TODO: Add an option for the user to manually choose the file directory
-    std::ifstream imgStream(imgPath, std::ios::binary);
+    std::ifstream imgStream(imgPath, std::ios::binary);                             //Open the file in binary
     int length = 10;
-    std::string binStr;
-    std::vector<std::string> hexArr;
+    std::string binStr;                                                             //Bytes are stored in this variable. It's a sting to make the output easier                                                     
 
     //TODO: Expand the list with more file formats
     //TODO: Improve the readablity of the below vector (this can be problematic once the list gets larger)
-    std::map<std::string, std::string> formats = {
+    std::map<std::string, std::string> formats = {                                  //Simple map with formats. Hex values are keys to easily check the file format
         {"FFD8FFE0", "JPEG"},
         {"89504E47", "PNG"},
         {"494433", "MP3"},
@@ -35,24 +31,23 @@ int main(int argc, char* argv[]){
 
     if (imgStream) {
         std::cout << "Opened the file" << std::endl;
-        std::cout << imgPath << std::endl;
-        char * buffer = new char[length]; // raw bytes are being stored here in an array
-        imgStream.read(buffer, length);
+        std::cout << imgPath << std::endl;                                          //Optional. It's for debuging
+        char * buffer = new char[length];                                           //Raw bytes are being stored here in an array
+        imgStream.read(buffer, length);                                             //Read the buffer with the lenght of "length" variable
         
         for (size_t i = 0; i >= 0; ++i) {
-            unsigned char byte = static_cast<unsigned char>(buffer[i]);
+            unsigned char byte = static_cast<unsigned char>(buffer[i]);             //This converts a pointer to normal char variable
 
             std::cout << "Byte: ";
-            for (int bit = 7; bit >= 0; --bit) {
+            for (int bit = 7; bit >= 0; --bit) {                                    //Counts descending because bytes are being read from right to left instead of left to right
                 std::cout << ((byte >> bit) &1);
                 binStr.push_back('0' + ((byte >> bit) &1));
             }
 
             std::cout << ' ';
-            std::cout << '\n';
+            std::cout << '\n';                                                      //Optional. Just a space for decorative purpouses
 
-            //WARNING: This may be a bit dangerous. Every time when format is not found a new element in std::map is being created. It can easlily cause errors!
-            if(formats[binutil::bintohex(binStr)] != ""){
+            if(formats[binutil::bintohex(binStr)] != ""){                           //WARNING: This may be a bit dangerous. Every time when format is not found a new element in std::map is being created. It can easlily cause errors!
                 break;
             }
         }
